c8f726e1b3b329cf523e79bdb3c0762ee8144937
José Luiz Tafarel <jltafarel@gmail.com>
2016-10-19 20:11:41 -0200

    Deploy settings

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -43,7 +43,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.apiBaseUrl = 'https://murmuring-spire-81606.herokuapp.com/api';
   }
 
   return ENV;
